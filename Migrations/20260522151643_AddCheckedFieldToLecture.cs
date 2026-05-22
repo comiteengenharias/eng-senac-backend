64ab91dc9df046a624b48443b18575f00d67b964
Rafael Pequino <o.rafaelpequino@gmail.com>
2026-05-22 12:18:50 -0300

    feat: adiciona verificação de frequencia nas palestras

---
+++ Migrations/20260522151643_AddCheckedFieldToLecture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EngenhariasSenac.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCheckedFieldToLecture : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF NOT EXISTS (
+                    SELECT 1 FROM sys.columns
+                    WHERE object_id = OBJECT_ID(N'[Lectures]') AND name = 'Checked'
+                )
+                BEGIN
+                    ALTER TABLE [Lectures] ADD [Checked] bit NOT NULL DEFAULT CAST(0 AS bit);
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Checked",
+                table: "Lectures");
+        }
+    }
+}
