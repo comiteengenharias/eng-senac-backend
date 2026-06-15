830f7ba174940db54dc707f5245b61e85eae32a2
Rafael Pequino <o.rafaelpequino@gmail.com>
2026-06-15 08:55:24 -0300

    feat: adiciona rotas de retorno das avaliações das empresas

--- Endpoints/GeneralEndpoints.cs
+++ Endpoints/GeneralEndpoints.cs
@@ -7,6 +7,10 @@ namespace EngenhariasSenac.Endpoints
         public static void AddGeneralEndpoints(this WebApplication app)
         {
             app.MapGet("/api/Public/Projects", GeneralController.GetProjects);
+
+            app.MapGet("/api/Public/company-assessments/{codCompany}", GeneralController.GetCompanyAssessments);
+
+            app.MapGet("/api/Public/companies", GeneralController.GetAllCompanies);
         }
     }
 }
